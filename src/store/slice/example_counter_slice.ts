adf36db128f72c6626804521a43e1ac048401a80
Engin Bolat <engi.bolat@gmail.com>
2025-02-15 14:52:05 +0300

    init

--- src/store/slice/example_counter_slice.ts
+++ src/store/slice/example_counter_slice.ts
@@ -1,28 +1,25 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 export interface IInitialState {
   val: number;
 }
 
 export const initialState: IInitialState = {
-    val:0,
+  val: 0,
 };
 
-
 const exampleCounterSlice = createSlice({
-    name:'exampleCounter',
-    initialState:initialState,
-    reducers: {
-        incrementCounter(state,action) {
-            state.val += action.payload;
-        },
-        decrementCounter(state,action) {
-            state.val -= action.payload;
-        },
+  name: 'exampleCounter',
+  initialState: initialState,
+  reducers: {
+    incrementCounter(state, action: PayloadAction<number>) {
+      state.val += action.payload;
     },
+    decrementCounter(state, action: PayloadAction<number>) {
+      state.val -= action.payload;
+    },
+  },
 });
 
-
-export const {incrementCounter,decrementCounter} = exampleCounterSlice.actions;
+export const {incrementCounter, decrementCounter} = exampleCounterSlice.actions;
 export default exampleCounterSlice.reducer;
-
